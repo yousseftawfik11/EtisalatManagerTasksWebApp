79f2913ccef698c914bae661f196c5051d0b73aa
yousseftawfik11 <67921054+yousseftawfik11@users.noreply.github.com>
2022-02-05 22:23:15 +0200

    manga

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
 
-
+session_start();
 session_destroy();
-// echo '<script>
-// window.location.href="index.php";
-// </script>';
+echo '<script>
+window.location.href="index.php";
+</script>';
 ?>
