3cf68c2c5f1066dafbceed496f54e55f1cb5b602
Youssef Amer <Youssef Amer@YOUSSEFAMER>
2022-02-14 15:10:51 +0200

    tornado logo

--- ManagerHome.php
+++ ManagerHome.php
@@ -39,7 +39,7 @@ if(!isset($_SESSION["username"])||$_SESSION["username"]!=5000){
 
 <body class="backgroundimage">
 <nav class="navbar navbar-expand-lg navbar-light  navBar-color" >
-<a class="navbar-brand navBar-color" href="#"><img class="logosize" src='images/horse.svg'><br><span class="logoText">Tornado</span></a>
+<a class="navbar-brand navBar-color" href="#"><img class="logosize" src='images/horse.svg'><span class="logoText">Tornado</span></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
